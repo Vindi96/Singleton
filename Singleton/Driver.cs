d5d5668ec08bdbcd059a405975adbbcabc697594
Vindi96 <vindya.madhushani96@gmail.com>
2021-01-06 23:32:10 +0530

    first commit

---
+++ Singleton/Driver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class Driver
+    {
+        private static Driver driver;
+        private Driver()
+        {
+
+        }
+
+        public static Driver Getter()
+        {
+            if(driver == null)
+            {
+                driver = new Driver();
+                return driver;
+            }
+            return driver;
+
+
+        }
+    }
+}
---
+++ Singleton/TestDriver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Singleton
+{
+    class TestDriver
+    {
+        static void Main(string[] args)
+        {
+            Driver obj1 = Driver.Getter();
+            Driver obj2 = Driver.Getter();
+
+            Console.WriteLine(obj1.GetHashCode());
+            Console.WriteLine(obj2.GetHashCode());
+        }
+    }
+    
+}
